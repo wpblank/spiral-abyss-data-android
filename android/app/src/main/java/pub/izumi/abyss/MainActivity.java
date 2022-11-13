5d9284a2bacd717dfa979e34060d750e5de02e2e
izumi <pyrdesu@outlook.com>
2022-11-13 18:57:06 +0800

    init

---
+++ android/app/src/main/java/pub/izumi/abyss/MainActivity.java
@@ -0,0 +1,6 @@
+package pub.izumi.abyss;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const String MIHOYO = 'https://bbs.mihoyo.com/ys/home/26';
+  static const String HOYOLAB = 'https://www.hoyolab.com/home';
+
+  String url = MIHOYO;
+
+  final controller = Completer<WebViewController>();
+
+  void selectServer() {
+    controller.future.then((web) {
+      url = HOYOLAB;
+      web.runJavascript("window.location.href = '$url'");
+    });
+  }
+
+  void upload() {
+    controller.future.then((web) {
+      String server;
+      if (url == MIHOYO) {
+        server = "";
+      } else if (url == HOYOLAB) {
+        server = "global/";
+      } else {
+        return;
+      }
+      web.runJavascript("javascript:(function () { "
+          "const s = document.createElement('script'); "
+          "s.src = 'https://abyss.izumi.pub/spiralAbyss/${server}js/local'; "
+          "document.body.append(s) })();");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("米游社数据上传"),
+      ),
+      body: WebView(
+        initialUrl: MIHOYO,
+        onWebViewCreated: (webViewController) {
+          controller.complete(webViewController);
+        },
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        shape: const CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+              onPressed: selectServer,
+              child: const Text('国服', style: TextStyle(color: Colors.black87)),
+            ),
+            const SizedBox(),
+            TextButton(
+              onPressed: selectServer,
+              child: const Text('国际服', style: TextStyle(color: Colors.black87)),
+            ),
+          ], //均分底部导航栏横向空间
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: upload,
+        child: Image.asset(
+          "images/upload.png",
+          width: 32,
+          height: 32,
+        ),
+      ),
+    );
+  }
+}
